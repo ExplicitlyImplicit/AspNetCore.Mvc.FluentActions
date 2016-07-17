4db01bee358a6ccaa8c395e66416fc596b42f4d1
Explicitly Implicit <explicitlyimplicit@zoho.com>
2016-07-17 08:37:20 +0200

    Moved EndpointCollection to separate file

--- src/ExplicitlyImpl.AspNetCore.Mvc.FluentEndpoints.Core/Endpoint.cs
+++ src/ExplicitlyImpl.AspNetCore.Mvc.FluentEndpoints.Core/Endpoint.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
@@ -8,50 +7,6 @@ using System.Linq.Expressions;
 
 namespace ExplicitlyImpl.AspNetCore.Mvc.FluentEndpoints
 {
-    public class EndpointCollection : IEnumerable<Endpoint>
-    {
-        public List<Endpoint> Endpoints { get; internal set; }
-
-        public EndpointCollection()
-        {
-            Endpoints = new List<Endpoint>();
-        }
-
-        public Endpoint Add(HttpMethod httpMethod, string url, string description = null)
-        {
-            var endpoint = new Endpoint(httpMethod, url, description);
-            Endpoints.Add(endpoint);
-            return endpoint;
-        }
-
-        public Endpoint Add(string url, HttpMethod httpMethod, string description = null)
-        {
-            var endpoint = new Endpoint(httpMethod, url, description);
-            Endpoints.Add(endpoint);
-            return endpoint;
-        }
-
-        public void Add(Endpoint endpoint)
-        {
-            Endpoints.Add(endpoint);
-        }
-
-        public void Add(IEnumerable<Endpoint> endpoints)
-        {
-            Endpoints.AddRange(endpoints);
-        }
-
-        public IEnumerator<Endpoint> GetEnumerator()
-        {
-            return Endpoints.GetEnumerator();
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
-        }
-    }
-
     public class EndpointUsingDefinition
     {
         public Type Type { get; set; }
