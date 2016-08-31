51b9486432cf6d5c4ae06aff40b419c6ab83969c
Explicitly Implicit <explicitlyimplicit@zoho.com>
2016-08-31 13:23:22 +0200

    Tests for parameterless actions

---
+++ test/ExplicitlyImpl.FluentActions.Test.UnitTests/BuilderTestsWithoutUsings.cs
@@ -0,0 +1,69 @@
+﻿using ExplicitlyImpl.AspNetCore.Mvc.FluentActions;
+using ExplicitlyImpl.FluentActions.Test.UnitTests.Controllers;
+using System;
+using Xunit;
+using static ExplicitlyImpl.AspNetCore.Mvc.FluentActions.FluentActionControllerDefinitionBuilder;
+
+namespace ExplicitlyImpl.FluentActions.Test.UnitTests
+{
+    public class BuilderTestsWithoutUsings
+    {
+        [Fact(DisplayName = "No handler, throws")]
+        public void FluentControllerBuilder_ThrowsOnFluentActionWithoutHandler()
+        {
+            var fluentAction = new FluentAction("/", HttpMethod.Get);
+
+            Assert.Throws<FluentActionValidationException>(() => BuilderTestUtils.BuildController(fluentAction));
+        }
+
+        [Fact(DisplayName = "No usings, returns string")]
+        public void FluentControllerBuilder_FluentActionWithoutUsingsAndReturnsString()
+        {
+            BuilderTestUtils.BuildActionAndCompareToStaticAction(
+                new FluentAction("/", HttpMethod.Get)
+                    .To(() => "Hello"), 
+                typeof(ParameterlessControllerReturnsString));
+        }
+
+        [Fact(DisplayName = "No usings, returns int")]
+        public void FluentControllerBuilder_FluentActionWithoutUsingsAndReturnsInt()
+        {
+            BuilderTestUtils.BuildActionAndCompareToStaticAction(
+                new FluentAction("/", HttpMethod.Get)
+                    .To(() => 13),
+                typeof(ParameterlessControllerReturnsInt));
+        }
+
+        [Fact(DisplayName = "No usings, returns guid")]
+        public void FluentControllerBuilder_FluentActionWithoutUsingsAndReturnsGuid()
+        {
+            BuilderTestUtils.BuildActionAndCompareToStaticAction(
+                new FluentAction("/", HttpMethod.Get)
+                    .To(() => new Guid("2a6d4959-817c-4514-90f3-52b518e9ddb0")),
+                typeof(ParameterlessControllerReturnsGuid));
+        }
+
+        [Fact(DisplayName = "No usings, returns enum")]
+        public void FluentControllerBuilder_FluentActionWithoutUsingsAndReturnsEnum()
+        {
+            BuilderTestUtils.BuildActionAndCompareToStaticAction(
+                new FluentAction("/", HttpMethod.Get)
+                    .To(() => ExampleEnumWithoutUsings.ExampleEnumValue2),
+                typeof(ParameterlessControllerReturnsEnum));
+        }
+
+        [Fact(DisplayName = "No usings, returns object")]
+        public void FluentControllerBuilder_FluentActionWithoutUsingsAndReturnsObject()
+        {
+            BuilderTestUtils.BuildActionAndCompareToStaticAction(
+                new FluentAction("/", HttpMethod.Get)
+                    .To(() => new ExampleClassWithoutUsings
+                    {
+                        StringField = "Hello",
+                        IntField = 14,
+                        StringProperty = "World!"
+                    }),
+                typeof(ParameterlessControllerReturnsObject));
+        }
+    }
+}
---
+++ test/ExplicitlyImpl.FluentActions.Test.UnitTests/Controllers/ParameterlessControllers.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace ExplicitlyImpl.FluentActions.Test.UnitTests.Controllers
+{
+    public class ParameterlessControllerReturnsString : Controller
+    {
+        [HttpGet]
+        public string HandlerAction()
+        {
+            return "Hello";
+        }
+    }
+
+    public class ParameterlessControllerReturnsInt : Controller
+    {
+        [HttpGet]
+        public int HandlerAction()
+        {
+            return 13;
+        }
+    }
+
+    public class ParameterlessControllerReturnsGuid : Controller
+    {
+        [HttpGet]
+        public Guid HandlerAction()
+        {
+            return new Guid("2a6d4959-817c-4514-90f3-52b518e9ddb0");
+        }
+    }
+
+    public enum ExampleEnumWithoutUsings
+    {
+        ExampleEnumValue1,
+        ExampleEnumValue2,
+        ExampleEnumValue3
+    }
+
+    public class ParameterlessControllerReturnsEnum : Controller
+    {
+        [HttpGet]
+        public ExampleEnumWithoutUsings HandlerAction()
+        {
+            return ExampleEnumWithoutUsings.ExampleEnumValue2;
+        }
+    }
+
+    public class ExampleClassWithoutUsings
+    {
+        public string StringField;
+
+        public int IntField;
+
+        public string StringProperty { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return obj is ExampleClassWithoutUsings &&
+                ((ExampleClassWithoutUsings)obj).StringField == StringField &&
+                ((ExampleClassWithoutUsings)obj).IntField == IntField &&
+                ((ExampleClassWithoutUsings)obj).StringProperty == StringProperty;
+        }
+    }
+
+    public class ParameterlessControllerReturnsObject : Controller
+    {
+        [HttpGet]
+        public ExampleClassWithoutUsings HandlerAction()
+        {
+            return new ExampleClassWithoutUsings
+            {
+                StringField = "Hello",
+                IntField = 14,
+                StringProperty = "World!"
+            };
+        }
+    }
+}
