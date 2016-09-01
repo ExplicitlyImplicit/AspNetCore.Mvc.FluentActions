eafb3d438bc770adb58d4b01b7f94744a48ea3ba
Explicitly Implicit <explicitlyimplicit@zoho.com>
2016-09-01 18:34:53 +0200

    Base tests for query string parameters

---
+++ test/ExplicitlyImpl.FluentActions.Test.UnitTests/BuilderTestsWithQueryStringParameters.cs
@@ -0,0 +1,55 @@
+﻿using ExplicitlyImpl.AspNetCore.Mvc.FluentActions;
+using ExplicitlyImpl.FluentActions.Test.UnitTests.Controllers;
+using System;
+using Xunit;
+
+namespace ExplicitlyImpl.FluentActions.Test.UnitTests
+{
+    public class BuilderTestsWithQueryStringParameters
+    {
+        [Fact(DisplayName = "1 query string parameter (string), returns string")]
+        public void FluentControllerBuilder_FluentActionUsingQueryStringParmeterReturnsString()
+        {
+            BuilderTestUtils.BuildActionAndCompareToStaticAction(
+                new FluentAction("/route/url", HttpMethod.Get)
+                    .UsingQueryStringParameter<string>("name")
+                    .To(name => $"Hello {name}!"),
+                typeof(ControllerWithQueryStringParameterReturnsString),
+                new object[] { "Charlie" });
+        }
+
+        [Fact(DisplayName = "1 query string parameter (string) with used default value, returns string")]
+        public void FluentControllerBuilder_FluentActionUsingQueryStringParmeterWithUsedDefaultValueReturnsString()
+        {
+            BuilderTestUtils.BuildActionAndCompareToStaticAction(
+                new FluentAction("/route/url", HttpMethod.Get)
+                    .UsingQueryStringParameter<string>("name", "Hanzel")
+                    .To(name => $"Hello {name}!"),
+                typeof(ControllerWithQueryStringParameterAndDefaultValueReturnsString),
+                new object[] { Type.Missing });
+        }
+
+        [Fact(DisplayName = "1 query string parameter (string) with unused default value, returns string")]
+        public void FluentControllerBuilder_FluentActionUsingQueryStringParmeterWithUnusedDefaultValueReturnsString()
+        {
+            BuilderTestUtils.BuildActionAndCompareToStaticAction(
+                new FluentAction("/route/url", HttpMethod.Get)
+                    .UsingQueryStringParameter<string>("name", "Hanzel")
+                    .To(name => $"Hello {name}!"),
+                typeof(ControllerWithQueryStringParameterAndDefaultValueReturnsString),
+                new object[] { "Charlie" });
+        }
+
+        [Fact(DisplayName = "2 query string parameters (string, identical), returns string")]
+        public void FluentControllerBuilder_FluentActionUsingTwoIdenticalQueryStringParmetersReturnsString()
+        {
+            BuilderTestUtils.BuildActionAndCompareToStaticAction(
+                new FluentAction("/route/url", HttpMethod.Get)
+                    .UsingQueryStringParameter<string>("name")
+                    .UsingQueryStringParameter<string>("name")
+                    .To((name1, name2) => $"Hello {name1}! I said hello {name2}!"),
+                typeof(ControllerWithTwoIdenticalQueryStringParametersReturnsString),
+                new object[] { "Charlie" });
+        }
+    }
+}
---
+++ test/ExplicitlyImpl.FluentActions.Test.UnitTests/Controllers/ControllersWithQueryStringParameters.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExplicitlyImpl.FluentActions.Test.UnitTests.Controllers
+{
+    public class ControllerWithQueryStringParameterReturnsString : Controller
+    {
+        [HttpGet]
+        [Route("/route/url")]
+        public string HandlerAction([FromQuery]string name)
+        {
+            return $"Hello {name}!";
+        }
+    }
+
+    public class ControllerWithQueryStringParameterAndDefaultValueReturnsString : Controller
+    {
+        [HttpGet]
+        [Route("/route/url")]
+        public string HandlerAction([FromQuery]string name = "Hanzel")
+        {
+            return $"Hello {name}!";
+        }
+    }
+
+    public class ControllerWithTwoIdenticalQueryStringParametersReturnsString : Controller
+    {
+        [HttpGet]
+        [Route("/route/url")]
+        public string HandlerAction([FromQuery]string name)
+        {
+            return $"Hello {name}! I said hello {name}!";
+        }
+    }
+}
