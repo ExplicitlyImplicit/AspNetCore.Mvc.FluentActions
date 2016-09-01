ff7a2b650988faff27d71f9fdb943fe51b5bb4c6
Explicitly Implicit <explicitlyimplicit@zoho.com>
2016-09-01 18:36:25 +0200

    Base tests for form parameters

---
+++ test/ExplicitlyImpl.FluentActions.Test.UnitTests/BuilderTestsWithForm.cs
@@ -0,0 +1,55 @@
+﻿using ExplicitlyImpl.AspNetCore.Mvc.FluentActions;
+using ExplicitlyImpl.FluentActions.Test.UnitTests.Controllers;
+using System;
+using Xunit;
+
+namespace ExplicitlyImpl.FluentActions.Test.UnitTests
+{
+    public class BuilderTestsWithForm
+    {
+        [Fact(DisplayName = "1 form (string), returns string")]
+        public void FluentControllerBuilder_FluentActionUsingFormReturnsString()
+        {
+            BuilderTestUtils.BuildActionAndCompareToStaticAction(
+                new FluentAction("/route/url", HttpMethod.Get)
+                    .UsingForm<string>()
+                    .To(name => $"Hello {name}!"),
+                typeof(ControllerWithFormReturnsString),
+                new object[] { "Charlie" });
+        }
+
+        [Fact(DisplayName = "1 form (string) with used default value, returns string")]
+        public void FluentControllerBuilder_FluentActionUsingFormWithUsedDefaultValueReturnsString()
+        {
+            BuilderTestUtils.BuildActionAndCompareToStaticAction(
+                new FluentAction("/route/url", HttpMethod.Get)
+                    .UsingForm<string>("Hanzel")
+                    .To(name => $"Hello {name}!"),
+                typeof(ControllerWithFormAndDefaultValueReturnsString),
+                new object[] { Type.Missing });
+        }
+
+        [Fact(DisplayName = "1 form (string) with unused default value, returns string")]
+        public void FluentControllerBuilder_FluentActionUsingFormWithUnusedDefaultValueReturnsString()
+        {
+            BuilderTestUtils.BuildActionAndCompareToStaticAction(
+                new FluentAction("/route/url", HttpMethod.Get)
+                    .UsingForm<string>("Hanzel")
+                    .To(name => $"Hello {name}!"),
+                typeof(ControllerWithFormAndDefaultValueReturnsString),
+                new object[] { "Charlie" });
+        }
+
+        [Fact(DisplayName = "2 form (string, identical), returns string")]
+        public void FluentControllerBuilder_FluentActionUsingTwoIdenticalFormsReturnsString()
+        {
+            BuilderTestUtils.BuildActionAndCompareToStaticAction(
+                new FluentAction("/route/url", HttpMethod.Get)
+                    .UsingForm<string>()
+                    .UsingForm<string>()
+                    .To((name1, name2) => $"Hello {name1}! I said hello {name2}!"),
+                typeof(ControllerWithTwoIdenticalFormsReturnsString),
+                new object[] { "Charlie" });
+        }
+    }
+}
---
+++ test/ExplicitlyImpl.FluentActions.Test.UnitTests/Controllers/ControllersWithForm.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExplicitlyImpl.FluentActions.Test.UnitTests.Controllers
+{
+    public class ControllerWithFormReturnsString : Controller
+    {
+        [HttpGet]
+        [Route("/route/url")]
+        public string HandlerAction([FromForm]string name)
+        {
+            return $"Hello {name}!";
+        }
+    }
+
+    public class ControllerWithFormAndDefaultValueReturnsString : Controller
+    {
+        [HttpGet]
+        [Route("/route/url")]
+        public string HandlerAction([FromForm]string name = "Hanzel")
+        {
+            return $"Hello {name}!";
+        }
+    }
+
+    public class ControllerWithTwoIdenticalFormsReturnsString : Controller
+    {
+        [HttpGet]
+        [Route("/route/url")]
+        public string HandlerAction([FromForm]string name)
+        {
+            return $"Hello {name}! I said hello {name}!";
+        }
+    }
+}
