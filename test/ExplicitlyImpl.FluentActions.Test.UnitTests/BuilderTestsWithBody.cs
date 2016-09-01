64ccfdb0b0a52b550b63904ab0493e4dd6516c2f
Explicitly Implicit <explicitlyimplicit@zoho.com>
2016-09-01 18:35:35 +0200

    Base tests for body parameters

---
+++ test/ExplicitlyImpl.FluentActions.Test.UnitTests/BuilderTestsWithBody.cs
@@ -0,0 +1,55 @@
+﻿using ExplicitlyImpl.AspNetCore.Mvc.FluentActions;
+using ExplicitlyImpl.FluentActions.Test.UnitTests.Controllers;
+using System;
+using Xunit;
+
+namespace ExplicitlyImpl.FluentActions.Test.UnitTests
+{
+    public class BuilderTestsWithBody
+    {
+        [Fact(DisplayName = "1 body (string), returns string")]
+        public void FluentControllerBuilder_FluentActionUsingBodyReturnsString()
+        {
+            BuilderTestUtils.BuildActionAndCompareToStaticAction(
+                new FluentAction("/route/url", HttpMethod.Get)
+                    .UsingBody<string>()
+                    .To(name => $"Hello {name}!"),
+                typeof(ControllerWithBodyReturnsString),
+                new object[] { "Charlie" });
+        }
+
+        [Fact(DisplayName = "1 body (string) with used default value, returns string")]
+        public void FluentControllerBuilder_FluentActionUsingBodyWithUsedDefaultValueReturnsString()
+        {
+            BuilderTestUtils.BuildActionAndCompareToStaticAction(
+                new FluentAction("/route/url", HttpMethod.Get)
+                    .UsingBody<string>("Hanzel")
+                    .To(name => $"Hello {name}!"),
+                typeof(ControllerWithBodyAndDefaultValueReturnsString),
+                new object[] { Type.Missing });
+        }
+
+        [Fact(DisplayName = "1 body (string) with unused default value, returns string")]
+        public void FluentControllerBuilder_FluentActionUsingBodyWithUnusedDefaultValueReturnsString()
+        {
+            BuilderTestUtils.BuildActionAndCompareToStaticAction(
+                new FluentAction("/route/url", HttpMethod.Get)
+                    .UsingBody<string>("Hanzel")
+                    .To(name => $"Hello {name}!"),
+                typeof(ControllerWithBodyAndDefaultValueReturnsString),
+                new object[] { "Charlie" });
+        }
+
+        [Fact(DisplayName = "2 body (string, identical), returns string")]
+        public void FluentControllerBuilder_FluentActionUsingTwoIdenticalBodysReturnsString()
+        {
+            BuilderTestUtils.BuildActionAndCompareToStaticAction(
+                new FluentAction("/route/url", HttpMethod.Get)
+                    .UsingBody<string>()
+                    .UsingBody<string>()
+                    .To((name1, name2) => $"Hello {name1}! I said hello {name2}!"),
+                typeof(ControllerWithTwoIdenticalBodysReturnsString),
+                new object[] { "Charlie" });
+        }
+    }
+}
---
+++ test/ExplicitlyImpl.FluentActions.Test.UnitTests/Controllers/ControllersWithBody.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExplicitlyImpl.FluentActions.Test.UnitTests.Controllers
+{
+    public class ControllerWithBodyReturnsString : Controller
+    {
+        [HttpGet]
+        [Route("/route/url")]
+        public string HandlerAction([FromBody]string name)
+        {
+            return $"Hello {name}!";
+        }
+    }
+
+    public class ControllerWithBodyAndDefaultValueReturnsString : Controller
+    {
+        [HttpGet]
+        [Route("/route/url")]
+        public string HandlerAction([FromBody]string name = "Hanzel")
+        {
+            return $"Hello {name}!";
+        }
+    }
+
+    public class ControllerWithTwoIdenticalBodysReturnsString : Controller
+    {
+        [HttpGet]
+        [Route("/route/url")]
+        public string HandlerAction([FromBody]string name)
+        {
+            return $"Hello {name}! I said hello {name}!";
+        }
+    }
+}
