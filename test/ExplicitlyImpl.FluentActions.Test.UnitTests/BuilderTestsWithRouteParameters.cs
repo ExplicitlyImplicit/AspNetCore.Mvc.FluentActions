24bc5d3a4e8ea37c0ef67dd0acc04eb032835cbf
Explicitly Implicit <explicitlyimplicit@zoho.com>
2016-09-01 18:35:15 +0200

    Base tests for route parameters

---
+++ test/ExplicitlyImpl.FluentActions.Test.UnitTests/BuilderTestsWithRouteParameters.cs
@@ -0,0 +1,55 @@
+﻿using ExplicitlyImpl.AspNetCore.Mvc.FluentActions;
+using ExplicitlyImpl.FluentActions.Test.UnitTests.Controllers;
+using System;
+using Xunit;
+
+namespace ExplicitlyImpl.FluentActions.Test.UnitTests
+{
+    public class BuilderTestsWithRouteParameters
+    {
+        [Fact(DisplayName = "1 route parameter (string), returns string")]
+        public void FluentControllerBuilder_FluentActionUsingRouteParmeterReturnsString()
+        {
+            BuilderTestUtils.BuildActionAndCompareToStaticAction(
+                new FluentAction("/route/{name}", HttpMethod.Get)
+                    .UsingRouteParameter<string>("name")
+                    .To(name => $"Hello {name}!"),
+                typeof(ControllerWithRouteParameterReturnsString),
+                new object[] { "Charlie" });
+        }
+
+        [Fact(DisplayName = "1 route parameter (string) with used default value, returns string")]
+        public void FluentControllerBuilder_FluentActionUsingRouteParmeterWithUsedDefaultValueReturnsString()
+        {
+            BuilderTestUtils.BuildActionAndCompareToStaticAction(
+                new FluentAction("/route/{name}", HttpMethod.Get)
+                    .UsingRouteParameter<string>("name", "Hanzel")
+                    .To(name => $"Hello {name}!"),
+                typeof(ControllerWithRouteParameterAndDefaultValueReturnsString),
+                new object[] { Type.Missing });
+        }
+
+        [Fact(DisplayName = "1 route parameter (string) with unused default value, returns string")]
+        public void FluentControllerBuilder_FluentActionUsingRouteParmeterWithUnusedDefaultValueReturnsString()
+        {
+            BuilderTestUtils.BuildActionAndCompareToStaticAction(
+                new FluentAction("/route/{name}", HttpMethod.Get)
+                    .UsingRouteParameter<string>("name", "Hanzel")
+                    .To(name => $"Hello {name}!"),
+                typeof(ControllerWithRouteParameterAndDefaultValueReturnsString),
+                new object[] { "Charlie" });
+        }
+
+        [Fact(DisplayName = "2 route parameters (string, identical), returns string")]
+        public void FluentControllerBuilder_FluentActionUsingTwoIdenticalRouteParmetersReturnsString()
+        {
+            BuilderTestUtils.BuildActionAndCompareToStaticAction(
+                new FluentAction("/route/{name}", HttpMethod.Get)
+                    .UsingRouteParameter<string>("name")
+                    .UsingRouteParameter<string>("name")
+                    .To((name1, name2) => $"Hello {name1}! I said hello {name2}!"),
+                typeof(ControllerWithTwoIdenticalRouteParametersReturnsString),
+                new object[] { "Charlie" });
+        }
+    }
+}
---
+++ test/ExplicitlyImpl.FluentActions.Test.UnitTests/Controllers/ControllersWithRouteParameters.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExplicitlyImpl.FluentActions.Test.UnitTests.Controllers
+{
+    public class ControllerWithRouteParameterReturnsString : Controller
+    {
+        [HttpGet]
+        [Route("/route/{name}")]
+        public string HandlerAction([FromRoute]string name)
+        {
+            return $"Hello {name}!";
+        }
+    }
+
+    public class ControllerWithRouteParameterAndDefaultValueReturnsString : Controller
+    {
+        [HttpGet]
+        [Route("/route/{name}")]
+        public string HandlerAction([FromRoute]string name = "Hanzel")
+        {
+            return $"Hello {name}!";
+        }
+    }
+
+    public class ControllerWithTwoIdenticalRouteParametersReturnsString : Controller
+    {
+        [HttpGet]
+        [Route("/route/{name}")]
+        public string HandlerAction([FromRoute]string name)
+        {
+            return $"Hello {name}! I said hello {name}!";
+        }
+    }
+}
