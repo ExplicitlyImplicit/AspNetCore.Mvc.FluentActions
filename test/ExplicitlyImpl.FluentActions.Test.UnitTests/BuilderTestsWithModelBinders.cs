da728eaf9700ce3b568b62d9bfb8a06a167583f0
Explicitly Implicit <explicitlyimplicit@zoho.com>
2016-09-01 19:09:52 +0200

    Base tests for model binders

---
+++ test/ExplicitlyImpl.FluentActions.Test.UnitTests/BuilderTestsWithModelBinders.cs
@@ -0,0 +1,56 @@
+﻿using ExplicitlyImpl.AspNetCore.Mvc.FluentActions;
+using ExplicitlyImpl.FluentActions.Test.UnitTests.Controllers;
+using Microsoft.AspNetCore.Mvc.Internal;
+using System;
+using Xunit;
+
+namespace ExplicitlyImpl.FluentActions.Test.UnitTests
+{
+    public class BuilderTestsWithModelBinders
+    {
+        [Fact(DisplayName = "1 model binder (string), returns string")]
+        public void FluentControllerBuilder_FluentActionUsingModelBinderReturnsString()
+        {
+            BuilderTestUtils.BuildActionAndCompareToStaticAction(
+                new FluentAction("/route/url", HttpMethod.Get)
+                    .UsingModelBinder<string>(typeof(NoOpBinder))
+                    .To(name => $"Hello {name}!"),
+                typeof(ControllerWithModelBinderReturnsString),
+                new object[] { "Charlie" });
+        }
+
+        [Fact(DisplayName = "1 model binder (string) with used default value, returns string")]
+        public void FluentControllerBuilder_FluentActionUsingModelBinderWithUsedDefaultValueReturnsString()
+        {
+            BuilderTestUtils.BuildActionAndCompareToStaticAction(
+                new FluentAction("/route/url", HttpMethod.Get)
+                    .UsingModelBinder<string>(typeof(NoOpBinder), "Hanzel")
+                    .To(name => $"Hello {name}!"),
+                typeof(ControllerWithModelBinderAndDefaultValueReturnsString),
+                new object[] { Type.Missing });
+        }
+
+        [Fact(DisplayName = "1 model binder (string) with unused default value, returns string")]
+        public void FluentControllerBuilder_FluentActionUsingModelBinderWithUnusedDefaultValueReturnsString()
+        {
+            BuilderTestUtils.BuildActionAndCompareToStaticAction(
+                new FluentAction("/route/url", HttpMethod.Get)
+                    .UsingModelBinder<string>(typeof(NoOpBinder), "Hanzel")
+                    .To(name => $"Hello {name}!"),
+                typeof(ControllerWithModelBinderAndDefaultValueReturnsString),
+                new object[] { "Charlie" });
+        }
+
+        [Fact(DisplayName = "2 model binders (string, identical), returns string")]
+        public void FluentControllerBuilder_FluentActionUsingTwoIdenticalModelBindersReturnsString()
+        {
+            BuilderTestUtils.BuildActionAndCompareToStaticAction(
+                new FluentAction("/route/url", HttpMethod.Get)
+                    .UsingModelBinder<string>(typeof(NoOpBinder))
+                    .UsingModelBinder<string>(typeof(NoOpBinder))
+                    .To((name1, name2) => $"Hello {name1}! I said hello {name2}!"),
+                typeof(ControllerWithTwoIdenticalModelBindersReturnsString),
+                new object[] { "Charlie" });
+        }
+    }
+}
---
+++ test/ExplicitlyImpl.FluentActions.Test.UnitTests/Controllers/ControllersWithModelBinders.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Internal;
+
+namespace ExplicitlyImpl.FluentActions.Test.UnitTests.Controllers
+{
+    public class ControllerWithModelBinderReturnsString : Controller
+    {
+        [HttpGet]
+        [Route("/route/url")]
+        public string HandlerAction([ModelBinder(BinderType = typeof(NoOpBinder))]string name)
+        {
+            return $"Hello {name}!";
+        }
+    }
+
+    public class ControllerWithModelBinderAndDefaultValueReturnsString : Controller
+    {
+        [HttpGet]
+        [Route("/route/url")]
+        public string HandlerAction([ModelBinder(BinderType = typeof(NoOpBinder))]string name = "Hanzel")
+        {
+            return $"Hello {name}!";
+        }
+    }
+
+    public class ControllerWithTwoIdenticalModelBindersReturnsString : Controller
+    {
+        [HttpGet]
+        [Route("/route/url")]
+        public string HandlerAction([ModelBinder(BinderType = typeof(NoOpBinder))]string name)
+        {
+            return $"Hello {name}! I said hello {name}!";
+        }
+    }
+}
