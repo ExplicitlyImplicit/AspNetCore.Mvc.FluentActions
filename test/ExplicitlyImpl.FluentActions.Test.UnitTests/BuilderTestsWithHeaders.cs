34313ad597900966d9035786ca0423a2ab90eb2d
Explicitly Implicit <explicitlyimplicit@zoho.com>
2016-09-01 19:09:39 +0200

    Base tests for header parameters

---
+++ test/ExplicitlyImpl.FluentActions.Test.UnitTests/BuilderTestsWithHeaders.cs
@@ -0,0 +1,55 @@
+﻿using ExplicitlyImpl.AspNetCore.Mvc.FluentActions;
+using ExplicitlyImpl.FluentActions.Test.UnitTests.Controllers;
+using System;
+using Xunit;
+
+namespace ExplicitlyImpl.FluentActions.Test.UnitTests
+{
+    public class BuilderTestsWithHeaders
+    {
+        [Fact(DisplayName = "1 header (string), returns string")]
+        public void FluentControllerBuilder_FluentActionUsingHeaderReturnsString()
+        {
+            BuilderTestUtils.BuildActionAndCompareToStaticAction(
+                new FluentAction("/route/url", HttpMethod.Get)
+                    .UsingHeader<string>("name")
+                    .To(name => $"Hello {name}!"),
+                typeof(ControllerWithHeaderReturnsString),
+                new object[] { "Charlie" });
+        }
+
+        [Fact(DisplayName = "1 header (string) with used default value, returns string")]
+        public void FluentControllerBuilder_FluentActionUsingHeaderWithUsedDefaultValueReturnsString()
+        {
+            BuilderTestUtils.BuildActionAndCompareToStaticAction(
+                new FluentAction("/route/url", HttpMethod.Get)
+                    .UsingHeader<string>("name", "Hanzel")
+                    .To(name => $"Hello {name}!"),
+                typeof(ControllerWithHeaderAndDefaultValueReturnsString),
+                new object[] { Type.Missing });
+        }
+
+        [Fact(DisplayName = "1 header (string) with unused default value, returns string")]
+        public void FluentControllerBuilder_FluentActionUsingHeaderWithUnusedDefaultValueReturnsString()
+        {
+            BuilderTestUtils.BuildActionAndCompareToStaticAction(
+                new FluentAction("/route/url", HttpMethod.Get)
+                    .UsingHeader<string>("name", "Hanzel")
+                    .To(name => $"Hello {name}!"),
+                typeof(ControllerWithHeaderAndDefaultValueReturnsString),
+                new object[] { "Charlie" });
+        }
+
+        [Fact(DisplayName = "2 headers (string, identical), returns string")]
+        public void FluentControllerBuilder_FluentActionUsingTwoIdenticalHeadersReturnsString()
+        {
+            BuilderTestUtils.BuildActionAndCompareToStaticAction(
+                new FluentAction("/route/url", HttpMethod.Get)
+                    .UsingHeader<string>("name")
+                    .UsingHeader<string>("name")
+                    .To((name1, name2) => $"Hello {name1}! I said hello {name2}!"),
+                typeof(ControllerWithTwoIdenticalHeadersReturnsString),
+                new object[] { "Charlie" });
+        }
+    }
+}
---
+++ test/ExplicitlyImpl.FluentActions.Test.UnitTests/Controllers/ControllersWithHeaders.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExplicitlyImpl.FluentActions.Test.UnitTests.Controllers
+{
+    public class ControllerWithHeaderReturnsString : Controller
+    {
+        [HttpGet]
+        [Route("/route/url")]
+        public string HandlerAction([FromHeader]string name)
+        {
+            return $"Hello {name}!";
+        }
+    }
+
+    public class ControllerWithHeaderAndDefaultValueReturnsString : Controller
+    {
+        [HttpGet]
+        [Route("/route/url")]
+        public string HandlerAction([FromHeader]string name = "Hanzel")
+        {
+            return $"Hello {name}!";
+        }
+    }
+
+    public class ControllerWithTwoIdenticalHeadersReturnsString : Controller
+    {
+        [HttpGet]
+        [Route("/route/url")]
+        public string HandlerAction([FromHeader]string name)
+        {
+            return $"Hello {name}! I said hello {name}!";
+        }
+    }
+}
