32e8b0911ecf576f5e62d31428d800009c2a8f7a
ExplicitlyImplicit <explicitlyimplicit@zoho.com>
2019-02-24 20:50:36 +0100

    Remake MvcTemplate sample as an unmodified .net core mvc web app

--- samples/MvcTemplate/README.md
+++
@@ -1,6 +0,0 @@
-# MVC Template Sample With Fluent Actions
-
-This project demonstrates an MCV template project that has integrated fluent actions.
-
-See [this commit](https://github.com/ExplicitlyImplicit/AspNetCore.Mvc.FluentActions/commit/68415086cc913ff73593771e615124f4eaa426b7) 
-to view the changes that were made to the original template.
--- samples/MvcTemplate/Startup.cs
+++ samples/MvcTemplate/Startup.cs
@@ -4,44 +4,42 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using ExplicitlyImpl.AspNetCore.Mvc.FluentActions;
 
 namespace MvcTemplate
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment env)
+        public Startup(IConfiguration configuration)
         {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-                .AddEnvironmentVariables();
-            Configuration = builder.Build();
+            Configuration = configuration;
         }
 
-        public IConfigurationRoot Configuration { get; }
+        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // Add framework services.
-            services.AddMvc().AddFluentActions();
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseBrowserLink();
             }
             else
             {
@@ -49,29 +47,13 @@ namespace MvcTemplate
             }
 
             app.UseStaticFiles();
+            app.UseCookiePolicy();
 
-            app.UseMvc();
-            app.UseFluentActions(actions =>
+            app.UseMvc(routes =>
             {
-                actions
-                    .RouteGet("/")
-                    .ToView("~/Views/Home/Index.cshtml");
-
-                actions
-                    .RouteGet("/Home/About")
-                    .UsingViewData()
-                    .Do(viewData => viewData["Message"] = "Your application description page.")
-                    .ToView("~/Views/Home/About.cshtml");
-
-                actions
-                    .RouteGet("/Home/Contact")
-                    .UsingViewData()
-                    .Do(viewData => viewData["Message"] = "Your contact page.")
-                    .ToView("~/Views/Home/Contact.cshtml");
-
-                actions
-                    .RouteGet("/Home/Error")
-                    .ToView("~/Views/Shared/Error.cshtml");
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
