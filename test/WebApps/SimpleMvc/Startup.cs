8b8739cb78a548d79af54af0ca56f86f37208f6a
ExplicitlyImplicit <explicitlyimplicit@zoho.com>
2019-05-19 09:36:10 +0200

    Set compatibility version to 2.2 in MVC test project

--- test/WebApps/SimpleMvc/Startup.cs
+++ test/WebApps/SimpleMvc/Startup.cs
@@ -28,7 +28,10 @@ namespace SimpleMvc
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().AddFluentActions();
+            services
+                .AddMvc()
+                .AddFluentActions()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             services.AddTransient<IUserService, UserService>();
         }
